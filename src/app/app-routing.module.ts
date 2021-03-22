850448398a2fd315d4feaf64498b5d04ce700e33
aTejada <atejada@sigcomt.com>
2021-03-22 01:40:27 -0500

    correcciones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
 import { RolComponent } from './auth/rol/rol.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'rol', pathMatch: 'full' },
   { path: 'rol', component: RolComponent },
+  { path: 'login', component: LoginComponent },
   {
     path: '',
     loadChildren: () => import('./layouts/layout.module').then((m) => m.LayoutModule),
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  visibleSidebar1;
+  
   constructor() { }
 
   ngOnInit(): void {
