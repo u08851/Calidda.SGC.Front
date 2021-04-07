6201a6b28706c55e51747022d90ea5995817ac28
anaisTe <atejada@sigcomt.com>
2021-04-07 09:16:06 -0500

    adm home

--- src/app/views/Manager/dashboard/dashboard.component.ts
+++ src/app/views/Manager/dashboard/dashboard.component.ts
@@ -3,14 +3,35 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent implements OnInit {  
+export class DashboardComponent implements OnInit {
+  // gráfico
+  basicData: any;
+  basicOptions: any;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    
+    this.basicData = {
+      labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'],
+      datasets: [
+        {
+          label: 'demo data 1',
+          backgroundColor: '#FAB200',
+          data: [65, 59, 80, 81, 56, 55, 40],
+        },
+        {
+          label: 'demo data 2',
+          backgroundColor: '#00A1DE',
+          data: [28, 48, 40, 19, 86, 27, 90],
+        },
+        {
+          label: 'demo data 3',
+          backgroundColor: '#435468',
+          data: [8, 78, 20, 15, 66, 37, 50],
+        },
+      ],
+    };
   }
-
 }
