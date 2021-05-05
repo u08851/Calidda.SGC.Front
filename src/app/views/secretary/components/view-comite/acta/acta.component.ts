855388de97e0cb92586699045b8a0fa584759936
Gamaliel <gabanto@sigcomt.com>
2021-05-05 10:34:25 -0500

    form acta

--- src/app/views/secretary/components/view-comite/acta/acta.component.ts
+++ src/app/views/secretary/components/view-comite/acta/acta.component.ts
@@ -7,9 +7,91 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActaComponent implements OnInit {
 
+  data: any[];
+  dataFActa: any[];
+  cols: any[];
+  colsFirmaActa:any[];
+  items: any[];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.data = DATA_ACTA;
+    this.dataFActa = DATA_FIRMA_ACTA;
+
+    this.cols = [
+      { header: 'Nombres y apellidos', field: 'names' },
+      { header: 'Tipo', field: 'tipo' },
+      { header: 'Cargo en el comité', field: 'cargoC' },
+      { header: 'Estado', field: 'status' },
+      { header: 'Forum', field: 'forum' }
+    ];
+    this.colsFirmaActa = [
+      { header: 'Nombres y apellidos', field: 'names' },
+      { header: 'Tipo', field: 'tipo' },
+      { header: 'Cargo en el comité', field: 'cargoC' },
+      { header: 'Estado', field: 'status' }
+    ];
   }
 
 }
+
+const DATA_ACTA = [
+  {
+    "names": "Rocio del Valle",
+    "tipo": "Titular",
+    "cargoC": "Presidente",
+    "status": {
+      'cod': '0',
+      'name': 'Confirmado'
+    }
+  },
+  {
+    "names": "Rocio del Valle",
+    "tipo": "Titular",
+    "cargoC": "Presidente",
+    "status": {
+      'cod': '1',
+      'name': 'Rechazado'
+    }
+  },
+  {
+    "names": "Rocio del Valle",
+    "tipo": "Titular",
+    "cargoC": "Presidente",
+    "status": {
+      'cod': '2',
+      'name': 'Pendiente'
+    }
+  },
+]
+
+const DATA_FIRMA_ACTA = [
+  {
+    "names": "Rocio del Valle",
+    "tipo": "Titular",
+    "cargoC": "Presidente",
+    "status": {
+      'cod': '0',
+      'name': 'Confirmado'
+    }
+  },
+  {
+    "names": "Rocio del Valle",
+    "tipo": "Titular",
+    "cargoC": "Presidente",
+    "status": {
+      'cod': '1',
+      'name': 'Rechazado'
+    }
+  },
+  {
+    "names": "Rocio del Valle",
+    "tipo": "Titular",
+    "cargoC": "Presidente",
+    "status": {
+      'cod': '2',
+      'name': 'Pendiente'
+    }
+  },
+]
--- src/app/views/secretary/components/view-comite/votacion/votacion.component.ts
+++ src/app/views/secretary/components/view-comite/votacion/votacion.component.ts
@@ -7,9 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VotacionComponent implements OnInit {
 
+  selectedValues: string;
+  data: any[] = [
+    {
+      "names": "Rocio del Valle",
+      "status": {
+        'cod': '2',
+        'name': 'voto'
+      },
+      "tipo": "Titular",
+      "respuesta": "SI"
+    },
+    {
+      "names": "Rocio del Valle",
+      "status": {
+        'cod': '2',
+        'name': 'voto'
+      },
+      "tipo": "Titular",
+      "respuesta": "SI"
+    },
+    {
+      "names": "Rocio del Valle",
+      "status": {
+        'cod': '2',
+        'name': 'voto'
+      },
+      "tipo": "Titular",
+      "respuesta": "SI"
+    }
+  ];
+
+  cols: any[];
+  items: any[];
+
   constructor() { }
 
   ngOnInit(): void {
-  }
 
+    this.cols = [
+      { header: 'Nombres y apellidos', field: 'names' },
+      { header: 'Estado', field: 'status' },
+      { header: 'Tipo', field: 'tipo' },
+      { header: 'Respuesta', field: 'respuesta'}
+    ];
+  }
 }
