69350ddfd5c70b3f653f6e5d18c7eab155c4b575
Gamaliel <gabanto@sigcomt.com>
2021-05-04 19:31:47 -0500

    Form infortamion basic

--- src/app/views/secretary/components/view-comite/basic-information/basic-information.component.ts
+++ src/app/views/secretary/components/view-comite/basic-information/basic-information.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./basic-information.component.scss']
 })
 export class BasicInformationComponent implements OnInit {
-
+  selectedTipo: string;
+  tipoMeet:any[];
+  date:Date;
   constructor() { }
 
   ngOnInit(): void {
+    this.tipoMeet = [
+      {cod: '01', name: 'Item 1'}
+    ]
   }
 
 }
