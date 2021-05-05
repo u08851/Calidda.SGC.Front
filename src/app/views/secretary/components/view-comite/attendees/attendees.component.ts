ec7d6ee611f95ae2bd7aa49ff169a4dd244eed08
Gamaliel <gabanto@sigcomt.com>
2021-05-04 20:35:26 -0500

    table asistentes

--- src/app/views/secretary/components/view-comite/attendees/attendees.component.ts
+++ src/app/views/secretary/components/view-comite/attendees/attendees.component.ts
@@ -7,9 +7,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AttendeesComponent implements OnInit {
 
+  selectedValues: string;
+  data: any[] = [
+    {
+      "names": "Rocio del Valle",
+      "tipo": "Titular",
+      "cargoC": "Presidente",
+      "status": {
+        'cod': '0',
+        'name': 'Confirmado'
+      },
+      "asistencia": "Presencial",
+    },
+    {
+      "names": "Rocio del Valle",
+      "tipo": "Titular",
+      "cargoC": "Presidente",
+      "status": {
+        'cod': '1',
+        'name': 'Rechazado'
+      },
+      "asistencia": "Presencial",
+    },
+    {
+      "names": "Rocio del Valle",
+      "tipo": "Titular",
+      "cargoC": "Presidente",
+      "status": {
+        'cod': '2',
+        'name': 'Pendiente'
+      },
+      "asistencia": "Presencial",
+    },
+  ];
+
+  cols: any[];
+  items: any[];
+
   constructor() { }
 
   ngOnInit(): void {
-  }
 
+    this.cols = [
+      { header: 'Nombres y apellidos', field: 'names' },
+      { header: 'Tipo', field: 'tipo' },
+      { header: 'Cargo en el comité', field: 'cargoC' },
+      { header: 'Estado', field: 'status' },
+      { header: 'Asistencia', field: 'asistencia' }
+    ];
+  }
 }
